b772ee64f6788591fbcb82a54f646f1544990f6d
Kaushik, Gunasekaran <gtskaushik@gmail.com>
2019-09-12 20:32:22 +0530

    Started working on error messages

---
+++ server_side/functions/src/config/Constants.ts
@@ -0,0 +1,7 @@
+export const errorCode = 501;
+export const errMsgs = {
+    login_failure_invalid_credentials : "Login Failed: Invalid Credentials",
+    login_failure_verification_incomplete : "Login Failed: Please complete email verification",
+    signup_success : "Please verify email to proceed with login",
+    signup_duplicate : "Email is already signed up. Verification email has been sent again to your email"
+}
